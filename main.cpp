b1d759ce279eff2b51740cf59db8525c39177a1b
The Cheaterman <the.cheaterman@gmail.com>
2022-01-19 03:15:27 +0100

    Add minimal (working) Python integration - loads python module from cwd

--- main.cpp
+++ main.cpp
@@ -1,4 +1,7 @@
+#include <Python.h>
 #include <sdk.hpp>
+
+#include "python_vars.h"
 
 
 struct PyOMPComponent final : IComponent {
@@ -17,12 +20,29 @@ struct PyOMPComponent final : IComponent {
     }
 
     void onInit(IComponentList* components) override {
+#ifndef WIN32
+        dlopen(PYTHON_LIBRARY, RTLD_GLOBAL | RTLD_LAZY);
+#endif
+
+        Py_SetPythonHome(PYTHON_HOME);
+        Py_Initialize();
+
+        PyObject* osModule = PyImport_ImportModule("os");
+        PyObject* cwdStr = PyObject_CallMethod(osModule, "getcwd", NULL);
+        Py_DECREF(osModule);
+
+        PyObject* sysPath = PySys_GetObject("path");
+        PyList_Append(sysPath, cwdStr);
+        Py_DECREF(cwdStr);
     }
 
     void onReady() override {
+        if(PyImport_ImportModule("python") == nullptr)
+            PyErr_Print();
     }
 
     void free() override {
+        Py_FinalizeEx();
         delete this;
     }
 
